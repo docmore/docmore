a95e7039a3da67b4118aff923f9a0e5681e43165
fireae <fireae@users.noreply.github.com>
2025-02-19 13:02:10 +0000

    ✅ Ready to clone and code.

---
+++ docmore/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for docmore."""
+
+from docmore.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from docmore.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "docmore"
